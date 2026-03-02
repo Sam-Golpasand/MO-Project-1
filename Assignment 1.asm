4821c84691a3981a990dc4fd74c122ef710d0967
Sam Golpasand <samgolpasand45@gmail.com>
2026-03-02 10:33:52 +0100

    Better comments

--- Assignment 1.asm
+++ Assignment 1.asm
@@ -5,17 +5,20 @@ LD R0, A ; Lower number
 LD R1, B ; Upper number
 
 X
+    ; Check if A - B is zero. If so, break to DONE
     NOT R2, R0
     ADD R2, R2, #1
-    ADD R2, R2, R1 ; Add R1 to R2 and store in R2   
+    ADD R2, R2, R1
     BRz DONE
     
-    ADD R1, R1, #-1 ; Remove 1 from R1 
+    ; De/increment both A and B
+    ADD R1, R1, #-1
     ADD R0, R0, #1
 
     BRNZP X
 
-DONE ST R1, C
+
+DONE ST R1, C ; Store the midpoint in C
 
 TRAP x25
 
